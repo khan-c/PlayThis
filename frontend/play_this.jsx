af25509d6ee337ebc3b5eb3a0199925f31e364de
Kyle Chen <kyle.h.chen@gmail.com>
2017-11-21 14:18:22 -0800

    Setup store

--- frontend/play_this.jsx
+++ frontend/play_this.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 
 // TESTING
 import * as SessionAPIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  // store
+  const store = configureStore();
   ReactDOM.render(<h1>PlayThis</h1>, root);
 
   //TESTING
+  window.getState = store.getState;
   window.login = SessionAPIUtil.login;
   window.signup = SessionAPIUtil.signup;
   window.logout = SessionAPIUtil.logout;
