e7d04d474b6c79eb5d7978bc22171c3126931dfb
Kyle Chen <kyle.h.chen@gmail.com>
2017-11-21 10:25:44 -0800

    Setup static page and configuration

---
+++ frontend/play_this.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  // store
+  ReactDOM.render(<h1>PlayThis</h1>, root);
+});
