fd15b4b44bb651b8fae392c31c0b921e398d34a8
Kyle Chen <kyle.h.chen@gmail.com>
2017-11-21 21:25:14 -0800

    Prepare for initial Heroku push

--- frontend/play_this.jsx
+++ frontend/play_this.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-// TESTING
-import * as SessionAPIUtil from './util/session_api_util';
-import { logout } from './actions/session_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store;
@@ -19,11 +15,4 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   ReactDOM.render(<Root store={ store } />, root);
-
-  //TESTING
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.login = SessionAPIUtil.login;
-  window.signup = SessionAPIUtil.signup;
-  window.logout = logout;
 });
