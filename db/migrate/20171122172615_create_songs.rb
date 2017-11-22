2a3c8a2d6e4e8bf6955b88879372360289729c6b
Kyle Chen <kyle.h.chen@gmail.com>
2017-11-22 09:34:48 -0800

    Run migrations for playlists and songs table

---
+++ db/migrate/20171122172615_create_songs.rb
@@ -0,0 +1,19 @@
+class CreateSongs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :songs do |t|
+      t.string :title, null: false
+      t.integer :artist_id, null: false
+      t.integer :album_id, null: false
+      t.integer :playlist_id
+      t.string :song_url, null: false
+
+      t.timestamps
+    end
+
+    add_index :songs, :artist_id
+    add_index :songs, :album_id
+    add_index :songs, :playlist_id
+    add_index :songs, [:title, :artist_id], unique: true
+    add_index :songs, [:title, :album_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121182742) do
+ActiveRecord::Schema.define(version: 20171122173253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "author_id", null: false
+    t.string "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_playlists_on_author_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "artist_id", null: false
+    t.integer "album_id", null: false
+    t.integer "playlist_id"
+    t.string "song_url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+    t.index ["playlist_id"], name: "index_songs_on_playlist_id"
+    t.index ["title", "album_id"], name: "index_songs_on_title_and_album_id", unique: true
+    t.index ["title", "artist_id"], name: "index_songs_on_title_and_artist_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
